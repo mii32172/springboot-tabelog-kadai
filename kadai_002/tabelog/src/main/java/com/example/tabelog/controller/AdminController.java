4a50d307d7e244bff29841a9f8aa2c6464ac7a19
mii32172 <mirai152732@gmail.com>
2024-12-22 14:00:13 +0900

    5回目のコミット4回目の課題提出

--- kadai_002/tabelog/src/main/java/com/example/tabelog/controller/AdminController.java
+++ kadai_002/tabelog/src/main/java/com/example/tabelog/controller/AdminController.java
@@ -40,28 +40,29 @@ public class AdminController {
 		this.restaurantService = restaurantService;
 		this.categoryRepository = categoryRepository;
 	}
-	
-	private String convertDayToJapanese(String englishDay) {
-	    switch (englishDay) {
-	        case "Monday":
-	            return "月曜日";
-	        case "Tuesday":
-	            return "火曜日";
-	        case "Wednesday":
-	            return "水曜日";
-	        case "Thursday":
-	            return "木曜日";
-	        case "Friday":
-	            return "金曜日";
-	        case "Saturday":
-	            return "土曜日";
-	        case "Sunday":
-	            return "日曜日";
-	        default:
-	            return englishDay;
-	    }
-	}
 
+	private String convertDayToJapanese(String englishDay) {
+		switch (englishDay) {
+		case "None":
+			return "定休日なし"; // 追加部分
+		case "Monday":
+			return "月曜日";
+		case "Tuesday":
+			return "火曜日";
+		case "Wednesday":
+			return "水曜日";
+		case "Thursday":
+			return "木曜日";
+		case "Friday":
+			return "金曜日";
+		case "Saturday":
+			return "土曜日";
+		case "Sunday":
+			return "日曜日";
+		default:
+			return englishDay;
+		}
+	}
 
 	@GetMapping
 	public String top() {
@@ -89,11 +90,11 @@ public class AdminController {
 	@GetMapping("/restaurants/{id}")
 	public String show(@PathVariable(name = "id") Integer id, Model model) {
 		Restaurant restaurant = restaurantRepository.getReferenceById(id);
-		
-	    String japaneseClosingDay = convertDayToJapanese(restaurant.getClosingDay());
+
+		String japaneseClosingDay = convertDayToJapanese(restaurant.getClosingDay());
 
 		model.addAttribute("restaurant", restaurant);
-		 model.addAttribute("japaneseClosingDay", japaneseClosingDay); // 日本語の曜日を追加
+		model.addAttribute("japaneseClosingDay", japaneseClosingDay); // 日本語の曜日を追加
 
 		return "admin/restaurants/show";
 	}
